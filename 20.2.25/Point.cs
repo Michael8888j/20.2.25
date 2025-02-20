c4ecb207ef3a33b554e042f1a60de6000770cb13
USER <USER@DESKTOP-N0LLAC0>
2025-02-20 09:20:04 +0200

    Add project files.

---
+++ 20.2.25/Point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._2._25
+{
+    internal class Point
+    {
+
+    }
+}
---
+++ 20.2.25/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._2._25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("Milk", 5.5, "Dairy"); //product בשם p1
+            Product p2 = new Product("Bread", "Bakery");
+            // Product p3 = new Product("Eggs", 3.5);
+            string name = p1.GetProductName();
+            string name2 = p2.GetProductName();
+            double price1 = p1.GetProductprice();
+            Console.WriteLine($" {name} {name2}");
+            double sum = p1.GetProductprice() + p2.GetProductprice();
+            p1.SetProductPrice(4.0); //הגדרת מחיר
+            Console.WriteLine(price1);
+            p1.UpdatePrice(20); 
+            Console.WriteLine(price1);
+        }
+        class Product
+        {
+            private string productName; //בניית מחלקה
+            private double price;
+            private string category;
+             public Product(string productName, double price, string category) //פעולה בונה
+            {
+                this.productName = productName;
+                this.price = price;
+                this.category = category;
+            }
+            public Product(string productName, string category) //פעולה בונה
+            {
+                this.productName = productName;
+                this.category = category;
+            }
+            public string GetProductName()
+            {
+                return productName;
+            }
+            public double GetProductprice()
+            {
+                return price;
+            }
+            public void SetProductPrice(double price)
+            {
+                this.price = price;
+            }
+            //פעולה שמקבלת אחוז מסויים ומעדכנת את    המחיר
+            public void UpdatePrice(double precent)
+            {
+                this.price = this.price + (this.price * precent / 100);
+               
+            }
+            public bool IsExpensive()
+            {
+                return price > 100; 
+            }
+            public override string ToString() //דרוס את ה tostring הקודם
+            {
+                return $"Product name: {productName}," + $"Price: {price}, Category: {category}";
+            }
+        }
+    }
+}
